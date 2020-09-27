1406aea2a85862dc922e2e97371ee26a6517208a
Nazar Tytskyi <nazar.tytskyi@thomsonreuters.com>
2020-09-28 02:43:33 +0300

    Refactor block component

--- src/js/components/Block.js
+++ src/js/components/Block.js
@@ -1,122 +1,148 @@
-import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
-import Codemirror from 'react-codemirror';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/markdown/markdown';
+import React, { useCallback, useState, useRef, useEffect } from "react";
+import { findDOMNode } from "react-dom";
+import Codemirror from "react-codemirror";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/markdown/markdown";
 import {
-    updateBlock, deleteBlock, moveBlockUp, moveBlockDown, editBlock
-} from '../actions';
+  updateBlock,
+  deleteBlock,
+  moveBlockUp,
+  moveBlockDown,
+  editBlock,
+} from "../actions";
 
-export default class Block extends Component {
+const useScrollIntoView = (elementToScroll) => {
+  useEffect(() => {
+    if (elementToScroll) {
+      elementToScroll.focus();
+      const domNode = findDOMNode(elementToScroll);
+      if (domNode.scrollIntoViewIfNeeded) {
+        findDOMNode(elementToScroll).scrollIntoViewIfNeeded(false);
+      }
+    }
+  });
+};
 
-    constructor(props) {
-        super(props);
-        this.enterEdit = this.enterEdit.bind(this);
-        this.textChanged = this.textChanged.bind(this);
-        this.getButtons = this.getButtons.bind(this);
-        this.deleteBlock = this.deleteBlock.bind(this);
-        this.moveBlockUp = this.moveBlockUp.bind(this);
-        this.moveBlockDown = this.moveBlockDown.bind(this);
+export default (props) => {
+  const [text, setText] = useState("");
+  const editarea = useRef(null);
+
+  useScrollIntoView(editarea);
+
+  const enterEdit = useCallback(
+    (e) => {
+      if (props.editable) {
+        e.stopPropagation();
+        const { dispatch, block } = this.props;
+
+        SetText(block.get("content"));
+      }
+      dispatch(editBlock(block.get("id")));
+    },
+    [props.editable, props.block, setText]
+  );
+
+  const textChanged = useCallback(
+    (text) => {
+      setText(text);
+    },
+    [setText]
+  );
+
+  const deleteBlock = useCallback(() => {
+    props.dispatch(deleteBlock(props.block.get("id")));
+  }, [props.block]);
+
+  const moveBlockUp = useCallback(() => {
+    props.dispatch(moveBlockUp(props.block.get("id")));
+  }, [props.block]);
+
+  const moveBlockDown = useCallback(() => {
+    props.dispatch(moveBlockDown(props.block.get("id")));
+  }, [props.block]);
+
+  const getButtons = useCallback(() => {
+    if (!props.editable) {
+      return null;
+    }
+    const buttons = [];
+
+    if (!props.isLast) {
+      buttons.push(
+        <i
+          className="fa fa-arrow-circle-o-down"
+          key="down"
+          onClick={this.moveBlockDown}
+          title="Move block down"
+        ></i>
+      );
     }
 
-    enterEdit(e) {
-        if (this.props.editable) {
-            e.stopPropagation();
-            const { dispatch, block } = this.props;
-            this.setState({
-                text: block.get('content')
-            });
-            dispatch(editBlock(block.get('id')));
-        }
+    if (!props.isFirst) {
+      buttons.push(
+        <i
+          className="fa fa-arrow-circle-o-up"
+          key="up"
+          onClick={this.moveBlockUp}
+          title="Move block up"
+        ></i>
+      );
     }
 
-    textChanged(text) {
-        this.setState({text});
-    }
+    buttons.push(
+      <i
+        className="fa fa-times-circle-o"
+        key="delete"
+        onClick={this.deleteBlock}
+        title="Remove block"
+      ></i>
+    );
 
-    componentDidUpdate() {
-        if (this.refs.editarea) {
-            this.refs.editarea.focus();
-            const domNode = findDOMNode(this.refs.editarea);
-            if (domNode.scrollIntoViewIfNeeded) {
-                findDOMNode(this.refs.editarea).scrollIntoViewIfNeeded(false);
-            }
-        }
-    }
+    return buttons;
+  }, [props.editable, props.isFirst, props.isLast]);
 
-    componentWillReceiveProps(newProps) {
-        if (this.props.editing && !newProps.editing &&
-            this.props.block.get('content') === newProps.block.get('content')) {
-            // If exiting edit mode, save text (unless it's an undo))
-            this.props.dispatch(
-                updateBlock(this.props.block.get('id'), this.state.text)
-            );
-        }
+  useEffect(() => {
+    if (
+      !props.editing //&&
+      //props.block.get("content") === newProps.block.get("content")
+    ) {
+      props.dispatch(updateBlock(props.block.get("id"), this.state.text));
     }
+  }, [props.editing, props.block]);
 
-    deleteBlock() {
-        this.props.dispatch(deleteBlock(this.props.block.get('id')));
-    }
+  const { block, editable, editing } = props;
 
-    moveBlockUp() {
-        this.props.dispatch(moveBlockUp(this.props.block.get('id')));
-    }
+  if (!(editable && editing)) {
+    return renderViewerMode();
+  }
 
-    moveBlockDown() {
-        this.props.dispatch(moveBlockDown(this.props.block.get('id')));
-    }
+  const isCodeBlock = block.get("type") === "code";
+  const options = {
+    mode: isCodeBlock ? "javascript" : "markdown",
+    theme: "base16-tomorrow-light",
+    lineNumbers: true,
+    indentUnit: 4,
+    extraKeys: {
+      Tab: (cm) => {
+        const spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
+        cm.replaceSelection(spaces);
+      },
+    },
+  };
 
-    getButtons() {
-        if (!this.props.editable) {
-            return null;
-        }
-        let buttons = [];
-        if (!this.props.isLast) {
-            buttons.push(
-                <i className="fa fa-arrow-circle-o-down" key="down"
-                    onClick={this.moveBlockDown} title="Move block down">
-                </i>
-            );
-        }
-        if (!this.props.isFirst) {
-            buttons.push(
-                <i className="fa fa-arrow-circle-o-up" key="up"
-                    onClick={this.moveBlockUp} title="Move block up">
-                </i>
-            );
-        }
-        buttons.push(
-            <i className="fa fa-times-circle-o" key="delete"
-                onClick={this.deleteBlock} title="Remove block">
-            </i>)
-        ;
-        return buttons;
-    }
-
-    render() {
-        const { block, editable, editing } = this.props;
-        if (!(editable && editing)) {
-            return this.renderViewerMode();
-        }
-        const isCodeBlock = block.get('type') === 'code';
-        const options = {
-            mode: isCodeBlock ? 'javascript' : 'markdown',
-            theme: 'base16-tomorrow-light',
-            lineNumbers: true,
-            indentUnit: 4,
-            extraKeys: {
-                Tab: (cm) => {
-                    var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
-                    cm.replaceSelection(spaces);
-                }
-            }
-        };
-        return (
-            <div className="edit-box" onClick={(e) => {e.stopPropagation()}}>
-                <Codemirror value={this.state.text} options={options}
-                    onChange={this.textChanged} ref="editarea" />
-            </div>
-        );
-    }
-
-}
+  return (
+    <div
+      className="edit-box"
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <Codemirror
+        value={text}
+        options={options}
+        onChange={textChanged}
+        ref={editarea}
+      />
+    </div>
+  );
+};
